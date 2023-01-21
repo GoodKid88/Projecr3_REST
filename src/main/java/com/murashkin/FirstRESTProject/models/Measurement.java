05b1e1eea9b9968f5776d08fd954c6be52fcd995
GoodKid <andrei.murashkin.1988@gmail.com>
2023-01-21 16:28:50 +0200

    add measurement model

---
+++ src/main/java/com/murashkin/FirstRESTProject/models/Measurement.java
@@ -0,0 +1,76 @@
+package com.murashkin.FirstRESTProject.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Measurement")
+public class Measurement {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "value")
+    @NotNull
+    @Min(message = "Value should be greater than -100", value = -100)
+    @Max(message = "Value should be less than 100", value = 100)
+    private double value;
+
+    @Column(name = "raining")
+    @NotNull
+    private boolean raining;
+
+    @Column(name = "measurement_date_time")
+    @NotNull
+    private LocalDateTime measurementDateTime;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name="sensor", referencedColumnName = "name")
+    private Sensor sensor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public LocalDateTime getMeasurementDateTime() {
+        return measurementDateTime;
+    }
+
+    public void setMeasurementDateTime(LocalDateTime measurementDateTime) {
+        this.measurementDateTime = measurementDateTime;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
